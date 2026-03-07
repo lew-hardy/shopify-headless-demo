c1aafced5ec4efe02b89de2635be501f2c62caa4
lew-hardy <lew.hardy@gmail.com>
2026-03-07 12:50:57 +0000

    simplify theme configs

--- lib/theme/get-theme-config.ts
+++ lib/theme/get-theme-config.ts
@@ -1,15 +1,6 @@
-import { themeConfig, type ThemeName } from "./config";
-
-import { theme as defaultTheme } from "themes/default/config";
-import { theme as electronicsTheme } from "themes/electronics/config";
-import { theme as fashionTheme } from "themes/fashion/config";
-
-const themeRegistry: Record<ThemeName, typeof defaultTheme> = {
- default: defaultTheme,
- fashion: fashionTheme,
- electronics: electronicsTheme,
-};
+import { themeConfig } from "./config";
+import { themeRegistry } from "./registry";
 
 export function getThemeConfig() {
- return themeRegistry[themeConfig.activeTheme] ?? themeRegistry.default;
+ return themeRegistry[themeConfig.activeTheme].config;
 }
